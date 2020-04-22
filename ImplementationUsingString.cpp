15235013bcc1838b20bcf4a3804046013022f0b9
Tashik Moin Sheikh <TashikMoinSheikh@Gmail.com>
2020-04-23 03:14:31 +0500

    Started Checking Sentence

--- ImplementationUsingString.cpp
+++ ImplementationUsingString.cpp
@@ -58,14 +58,47 @@ class Context_Free_Grammar
     Word_Node *Sentence ;
     int Sentence_Length ;
 
-    bool Index_Check(int i) 
+
+
+    Context_Free_Grammar()
     {
-        if ( i >= this->Sentence_Length ) 
+        string _Sentence ;
+        getline( cin , _Sentence ) ;
+        this->Sentence_Length = Total_Words(_Sentence) + 1 ;
+        char TokenString[this->Sentence_Length * 20 ] ;
+        strcpy( TokenString , _Sentence.c_str()) ;
+        char *Token = strtok( TokenString , " "); 
+        this->Sentence = new Word_Node[this->Sentence_Length] ;
+        int i = 0 ;
+        while (Token != NULL) 
+        { 
+            this->Sentence[i].Word = Token ;
+            cout<<this->Sentence[i].Word<<endl;
+            Token = strtok(NULL, " "); 
+            i++ ;
+        } 
+        this->Assign_Types() ;
+        cout<<endl ;
+        for( int i = 0 ; i < this->Sentence_Length ; ++i )
         {
-            return false;
+            cout<<"Word  =  "<<this->Sentence[i].Word<<"    Type  =  "<<this->Sentence[i].Type<<endl ;
         }
-        return true;
+        if( Check_Sentence_Structure() )
+        {
+            cout<<"\nValid Sentence Structure Following All Grammar Rules" ;
+        }
+        else
+        {
+            cout<<"\nInvalid Sentence Structure Violating Grammar Rules" ;
+        }
+        
     }
+
+    ~Context_Free_Grammar()
+    {
+        delete[] this->Sentence ;
+    }
+
 
     string Find_Type( string __Word )
     {
@@ -176,6 +209,7 @@ class Context_Free_Grammar
         exit(1) ;
     }
 
+
     void Assign_Types()
     {
         string __Type ;
@@ -187,35 +221,48 @@ class Context_Free_Grammar
         }
     }
 
-    Context_Free_Grammar()
+    bool Check_Sentence_Structure( int i = 0 , int j = 0 , string &NP , string &VP , string &PP )
     {
-        string _Sentence ;
-        getline( cin , _Sentence ) ;
-        this->Sentence_Length = Total_Words(_Sentence) + 1 ;
-        char TokenString[this->Sentence_Length * 20 ] ;
-        strcpy( TokenString , _Sentence.c_str()) ;
-        char *Token = strtok( TokenString , " "); 
-        this->Sentence = new Word_Node[this->Sentence_Length] ;
-        int i = 0 ;
-        while (Token != NULL) 
-        { 
-            this->Sentence[i].Word = Token ;
-            cout<<this->Sentence[i].Word<<endl;
-            Token = strtok(NULL, " "); 
-            i++ ;
-        } 
-        this->Assign_Types() ;
-        cout<<endl ;
-        for( int i = 0 ; i < this->Sentence_Length ; ++i )
+        if( this->Sentence[i].Type == "PRONOUN" || this->Sentence[i].Type == "PROPERNOUN" || this->Sentence[i].Type == "DETERMINER")
         {
-            cout<<"Word  =  "<<this->Sentence[i].Word<<"    Type  =  "<<this->Sentence[i].Type<<endl ;
+            if( this->Sentence[i].Type == "DETERMINER" && this->Sentence[i+1].Type == "NOUN" && this->Sentence[i+2].Type == "NOUN" )
+            {
+                NP = NP + " " + this->Sentence[i].Word + " " + this->Sentence[i+1].Word + " " + this->Sentence[i+2].Word ;
+                Check_Sentence_Structure( i+3 , j , NP , VP , PP ) ;                
+            }
+            else if( this->Sentence[i].Type == "DETERMINER" && this->Sentence[i+1].Type == "NOUN" )
+            {
+                NP = NP + " " + this->Sentence[i].Word + " " + this->Sentence[i+1].Word ;
+                Check_Sentence_Structure( i+2 , j , NP , VP , PP ) ;
+            }
+            else
+            {
+                NP = NP + " " + this->Sentence[i].Word ;
+                Check_Sentence_Structure( i+1 , j , NP , VP , PP ) ;
+            }
+            return true ;
         }
+        else 
+        {
+            if( VP == "" && PP == "" )
+            {
+                NP = "" ;
+            }
+            else if ( NP != "" && VP != "" && PP == "" )
+            {
+                cout<<NP<<" VP" ;
+                NP = "" ;
+            }
+            else if ( NP != "" && PP != "" )
+            {
+                cout<<NP<<" PP" ;
+                NP = "" ;
+            }
+        }
+        
     }
 
-    ~Context_Free_Grammar()
-    {
-        delete[] this->Sentence ;
-    }
+
 
 
 } ;
---
+++ Test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    string x = "" ;
+    x = x + " " + " Hello " + " Tashik " ;
+    cout<<x ;
+}
